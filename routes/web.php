e56e0d5d2172d41c46599e251009ce62fbef3650
Ifelere Oyetola <ifelere@gmail.com>
2023-01-24 18:29:19 +0100

    Customized welcome view blog components and inserted blog routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogsController;
+use App\Http\Controllers\Admin\BlogsController as AdminBlogsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,10 +24,19 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+Route::resource("blogs", BlogsController::class)->only(['index', 'show']);
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    
+
+    Route::prefix('admin')->name('admin.')->group(function () {
+        Route::resource('blogs', AdminBlogsController::class);
+    });
 });
 
 require __DIR__.'/auth.php';
